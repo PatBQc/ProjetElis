be737263d33813ab52fd5d38eff4db18dfbcbcb8
PatBQc <pbelanger@hotmail.com>
2023-11-30 21:43:33 -0500

    Ajout de commentaires

--- start.py
+++ start.py
@@ -8,6 +8,12 @@ import speech_recognition as sr
 import re
 import asyncio
 
+# On a ajouté une fonction main() "asynchrone"
+# Par convention, c'est comme ça que l'on appelle la fonction qui sera appelé au démarage de l'application.
+# La fonction principale, c'est donc la fonction "main".
+# Elle est "asynchrone", ça veut dire que l'on va pouvoir dans cette fonction faire plus d'une chose en même temps!
+# Par exemple, on va écouter la première phrase de la réponse "en même temps" que l'on va aller chercher la prochaine
+# phrase à écouter.  Comme ça, ça nous permet d'avoir la réponse qui commence à parler beaucoup plus vite.
 async def main():
 
     # Ici c'est notre clé top secrète pour appeler ChatGPT.  
@@ -21,36 +27,58 @@ async def main():
     experiment_folder = os.path.join("experiments", datetime_folder)
     os.mkdir(experiment_folder)
 
+    # Ceci, c'est une fonction qui sert à écouter au micro et à tout convertir en texte une fois que 
+    # l'enfant à dit "stop".  Il peut prendre son temps pour parler et même si il y a des blancs, ça 
+    # va continuer d'enregistrer jusqu'à ce qu'on entende le mot clé "stop".
     def listen_and_transcribe():
-        # Initialize recognizer
+        # Pour se faire, on va utiliser un objet pour reconaître la voix.  C'est notre "recognizer"
         recognizer = sr.Recognizer()
 
+        # Puis, on va se garder une variable qui va contenir tout le texte que l'on aura reconnu du micro.        
         text = ""
 
-        # Start listening
+        # Ici on commence à écouter au micro.  On dit donc que l'on veut utiliser le micro comme source
         with sr.Microphone() as source:
-            print("Adjusting ambient noise.")
+            
+            # Avec notre micro, on va commencer par "écouter" le son ambiant pour faire en sorte que lorsque
+            # l'on parle, on est capable de reconnaître la voix de tous les bruits de fond.
+            print("Ajustement du son ambiant.")
             recognizer.adjust_for_ambient_noise(source)
-            print("Listening... Say 'STOP' to end.")
+
+            # Et maintenant, on commence la vrai saissie pour écouter ce qui se dit et le convertir en texte.
+            print("En écoute... Dites 'STOP' pour finir ce que vous avez à dire.")
+
+            # On va faire une boucle qui va prendre chaque "segment" de conversation et les reconnaître, tant que
+            # nous n'avons pas entendu "STOP".  Un segment, c'est quand quelqu'un fait une pause en parlant...
+            # ...et à chaque fois qu'il y a une pause, on en profite pour convertir ce qu'on vient d'entendre en texte
             while True:
-                # Listen for audio
+                
+                # On se met en mode écoute du microphone
                 audio = recognizer.listen(source)
 
+                # Puis on essaie de reconnaître ce qui a été dit.  
                 try:
-                    # Perform STT
+                    # C'est notre opération de convertir la voix en texte, 
+                    # ou comme on appelle en anglais le Speach-To-Text (STT)
                     text = text + " " + recognizer.recognize_google(audio, language="fr-FR")
-                    print(f"Recognized: {text}")
+                    print(f"Texte reconnu: {text}")
 
-                    # Check if the keyword 'STOP' is in the recognized text
+                    # Puis on regarde si le mot clé "STOP" est dans le texte reconnu
                     if "STOP" in text.upper():
-                        print("Stopping...")
+                        # Si on l'a trouvé, on va arrêter l'écoute du microphone
+                        print("On arrête l'écoute...")
+
+                        # On va dire que notre fonction retourne tout ce qu'il y avait comme texte de 
+                        # détecté "avant" le mot clé "STOP"
                         return text.split("stop")[0]
                         break
 
+                # Mais quand on s'essaie, c'est possible que ça ne fonctionne pas.  Si c'est le cas, on 
+                # va dire que l'on a eu un cas "d'exception" qui n'est pas ce que l'on attendait.
                 except sr.UnknownValueError:
-                    print("Could not understand audio")
+                    print("Exception: Could not understand audio")
                 except sr.RequestError as e:
-                    print(f"Could not request results; {e}")
+                    print(f"Exception: Could not request results; {e}")
 
     # Maintenant, premier concept quand on parle à ChatGPT.  On fait des "PROMPTs", c'est à dire des messages.
     # On va se faire une liste de message que l'on s'échange.  En gros ce sont les messages de ChatGPT, les miens, ChatGPT, les miens, ...
@@ -91,19 +119,35 @@ async def main():
         prompts.append({"role": "assistant", "content": answer})
         print()
 
+        # De la réponse que l'on a reçu, on va séparer chaque phrase.  
+        # split_answers est une liste de toute les phrases.  
+        # Ça va nous permettre de les lire une après l'autre et d'avoir la réponse plus rapidement pour l'enfant.
+        # Par exemple, si on avait 5 paragraphes de réponses avec un total de 25 phrases, ça pourrait prendre 
+        # 30 secondes recevoir la réponse de tout l'audio à lire.  Mais si on prend juste la première phrase,
+        # on a peut-être la réponse en 2 secondes et pendant qu'on va la faire jouer, en même temps on va aller chercher
+        # la prochaine phrase à lire pour qu'elle soit disponible quand on va avoir finit d'écouter la première.
+        # C'est ce qu'on pourrait appeler un truc ou une astuce pour améliorer notre expérience.
+        # On peut aussi appeler ça dans le jargon, une heuristique.  
+        # Le fait de pouvoir aller chercher le contenu à lire en petit morceau plutôt que tout d'un coup, on appelle
+        # ça du "streaming".  On peut donc dire que l'on a codé une heuristique de streaming !
         split_answers = re.findall(r'[^.!?]*[.!?]', answer)
 
+        # On va se garder un index que quelle phrase (quelle partie de la réponse) nous sommes entrain de lire.
         answer_part_index = 0
 
+        # Et on va se garder une variable qui sert à contrôler notre lecture.  Ce sera task_player
         task_player = asyncio.sleep(0)
 
+        # On se fait une boucle pour passer chaque phrase une à la suite de l'autre.
         for answer_part in split_answers:
             # Ici c'est le code pour transformer le texte en son.  On appelle ça du TTS (text-to-speech)
             # On va prendre la dernière chose que nous a dit ChatGPT et on va demander à un site web de OpenAI
             # de nous le convertir dans un fichier audio, comme un fichier mp3
             response = client.audio.speech.create(
                 model="tts-1",
-                voice="echo",
+                # la liste des voix est disponible au https://platform.openai.com/docs/guides/text-to-speech
+                # Présentement, on peut choisir entre: alloy, echo, fable, onyx, nova, ou shimmer
+                voice="echo", 
                 response_format="mp3",
                 input=answer_part
                 )
@@ -112,15 +156,18 @@ async def main():
             filename = os.path.join(experiment_folder, str(len(prompts)).zfill(4) + "-answer (" + str(answer_part_index).zfill(2) + ").mp3")
             response.stream_to_file(filename)
         
+            # Maintenant que l'on a reçu notre réponse de l'audio de la prochaine phrase, on va attendre "await"
+            # que la phrase précédente qui est entrain d'être lu soit fini d'être dite.
             await task_player
 
-            # Et on va le faire jouer pour l'entendre dans le haut parleur
+            # Et on va maintenant faire jouer la nouvelle phrase pour l'entendre dans le haut parleur
             task_player = asyncio.create_task(asyncio.to_thread(playsound, filename))
-            await asyncio.sleep(0)
+            await asyncio.sleep(0) # cette ligne est seulement là pour forcer à commencer la lecture tout de suite
             
-
+            # et on ajoute 1 à notre variable qui compte on a lu combien de phrase.
             answer_part_index += 1
 
+        # En sortant de notre boucle qui lit chaque phrase, on va s'assurer que l'on a bien lu la dernière phrase au complet.
         await task_player
 
         # Maintenant, c'est le temps de demander à l'enfant ce qu'il veut dire
@@ -161,4 +208,6 @@ async def main():
             # On met la réponse de ChatGPR dans la variable "answer", et on va ensuite recommencer notre boucle !
             answer = gpt_client.choices[0].message.content
 
+# Ici, c'est la première ligne de code qui sera exécuter dans notre application.
+# C'est elle qui va aller exécuter la fonction principale, c'est à dire "main()" que l'on a défini au début de notre code.
 asyncio.run(main())
